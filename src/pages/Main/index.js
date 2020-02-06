a7967dc2c0f84e2eab6ddfdcb156bcb429a21897
juniorlourenco <juniorminaj101@gmail.com>
2020-02-06 08:35:38 -0400

    commit 1

---
+++ src/pages/Main/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
+
+import {Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation,
+} from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+
+      <Content>
+        <Menu />
+
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebica de Junior Lourenço da Silva
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+      <Tabs />
+    </Container>
+  );
+}
